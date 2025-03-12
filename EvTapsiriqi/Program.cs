3b7b9c7efd5343ec8c7753bda94f4948358660c7
XumarSefereliyeva <xumarsefereliyeva@gmail.com>
2025-03-12 17:37:50 +0300

    homeworkadded

---
+++ EvTapsiriqi/Program.cs
@@ -0,0 +1,250 @@
+﻿using System.ComponentModel.Design;
+
+namespace EvTapsiriqi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            int[] arrey = { 5, 8, 9, 3, 6, 5 };
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < arrey.Length; i++)
+            {
+                if (n>arrey[i])
+                {
+                    Console.WriteLine(true);
+                }
+                else
+                {
+                    Console.WriteLine(false);
+                }
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+                int[] arrey = { 6, 9, 3, 5, 7, };
+                int enboyuk = arrey[0];
+                for(int i = 0; i < arrey.Length; i++)
+                {
+                    if (enboyuk < arrey[i])
+                    {
+                        enboyuk = arrey[i];
+                    }
+                    Console.WriteLine(enboyuk);
+                }
+           */
+
+
+
+
+
+
+
+
+
+
+
+            /*
+
+                int num = int.Parse(Console.ReadLine());
+                int qaliq;
+                int reverse = 0;
+                while (num != 0)
+                {
+                    qaliq = num % 10;
+                    reverse = (reverse * 10) + qaliq;
+                    num /= 10;
+                }
+                Console.WriteLine(reverse);
+            }
+
+            */
+
+
+
+
+
+
+
+
+
+
+
+            /*
+            int num = 7;
+            int faktorial = 1;
+            for(int i = 0; i <= num; i++)
+            {
+                faktorial *= 1;
+            }
+            Console.WriteLine(faktorial);
+        }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+                    int num = 97;
+                    int qaliq;
+                    int hasil = 1;
+                        while (num != 0)
+                        {
+                            qaliq = num % 10;
+                            hasil *= qaliq;
+                            num /= 10;
+                        }
+                        Console.WriteLine(hasil);
+                }
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+            int eded = 67;
+            bool murekkebeded = false;
+            for (int i = 2; i < eded; i++)
+            {
+                if (eded % i == 0)
+                {
+                    murekkebeded = true;
+                }
+                Console.WriteLine(eded);
+            }
+
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+            string[] arrey = { "Lorem", "Ipsum" };
+            string beraber;
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                if (arrey[i] == "Ipsum")
+                {
+                    beraber = arrey[i];
+                }
+                Console.WriteLine(arrey[i]);
+            }
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+            string soz = "Salam";
+            char herf = 'a';
+            bool herfvar = false;
+            foreach (char a in soz)
+            {
+                if (a == herf)
+                {
+                    herfvar = true;
+                }
+                Console.WriteLine(herfvar);
+
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+
+            string str = Console.ReadLine();
+            string reversedstr = "";
+            bool error = false;
+            for(int i= str.Length-1;i>-1;i--)
+            
+            {
+                    if (!error)
+                {
+                    reversedstr += str[i];
+                }
+                Console.WriteLine(reversedstr);
+            }
+
+            */
+
+
+
+
+
+
+
+
+
+
+
+/*
+            string[] arrey = { "First", "Second", "Third" };
+            for(int i = arrey.Length - 1; i > -1; i--)
+            {
+                Console.WriteLine(arrey[i]);
+            }
+
+
+            */
+
+            }
+        }
+    }
+
+
